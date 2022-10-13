852d3ca661394e454d387d81788872d4cc8fea5d
Vladyslav Ovchynnykov <10961029+pythad@users.noreply.github.com>
2022-10-13 19:15:34 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-module "m" {
-	source  = "f7d0748b41b1.test-env.scalr.com/env-svrcnchebt61e30/m/local"
+module "local" {
+	source  = "f7d0748b41b1.test-env.scalr.com/env-svrcnchebt61e30/scalr/local"
 	version = "1.0.1"
 }
