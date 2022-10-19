5ccd237a1e5efa2d0d0e88d835818718751ea11c
Vladyslav Ovchynnykov <10961029+pythad@users.noreply.github.com>
2022-10-19 12:36:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,14 @@
-terraform {
-  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
-  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
-  # forwards compatible with 0.13.x code.
-  required_version = ">= 0.12.26"
+resource "null_resource" "env_timeout" {
+  count = 1
+  triggers = {
+    run_id = var.run_id
+  }
+  provisioner "local-exec" {
+    command = "env"
+  }
 }
 
-# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
-output "hello_world" {
-  value = "Hello, World!"
+variable "run_id" {
+  default = "run_id_2"
+  sensitive = true
 }
