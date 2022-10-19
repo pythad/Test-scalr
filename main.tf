f39e30b58eac8c1120c103e6a4bda5d66e3a6741
Vladyslav Ovchynnykov <10961029+pythad@users.noreply.github.com>
2022-10-19 12:05:35 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
-module "local" {
-	source  = "f7d0748b41b1.test-env.scalr.com/env-svrcnchebt61e30/local/scalr"
-	version = "1.0.2"
+terraform {
+  # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.13.x code.
+  required_version = ">= 0.12.26"
 }
 
-module "local2" {
-	source  = "f7d0748b41b1.test-env.scalr.com/env-svrcnchebt61e30/local/scalr"
-	version = "2.0.0"
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, World!"
 }
