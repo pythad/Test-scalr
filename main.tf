665772a4c1c8c00ce1fd88bd0c9146dbe9aeb88b
Vladyslav Ovchynnykov <10961029+pythad@users.noreply.github.com>
2023-01-05 10:15:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-module "local" {
-	source  = "mainiacp.org9.te.scalr-labs.net/env-svrcnchebt61e30/local/scalr"
-}
-  
- resource "null_resource" "resource1" {
+resource "null_resource" "resource1" {
   count = 1
   provisioner "local-exec" {
     command = "pwd"
