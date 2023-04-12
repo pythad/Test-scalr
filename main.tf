2c6dbdb137d0bf81bc8f19da602f68242341557e
Vladyslav Ovchynnykov <10961029+pythad@users.noreply.github.com>
2023-04-12 08:57:27 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,14 @@
-resource "null_resource" "resource_main"{
-  count = 10000
-  provisioner "local-exec" {
-    command = "pwd"
+resource "random_integer" "int-xc90" {
+  count = 20000
+  min = 2
+  max = 11
+
+  keepers = {
+    run_id = var.run_id
   }
+}
+
+variable "run_id" {
+  default = "run_id_2"
+  sensitive = true
 }
